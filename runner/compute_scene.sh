d0a69291ad23979be50838cd1da0180e79b30b23
Thomas Rouch <thom01.rouch@gmail.com>
2022-05-19 13:48:04 +0200

    :sparkles: update dockerfile and readme

--- runner/compute_scene.sh
+++ runner/compute_scene.sh
@@ -4,4 +4,4 @@ REPO_DIR=$(dirname ${0})
 workspaceRoot="$( cd ${REPO_DIR} >/dev/null 2>&1 && pwd )"
 cd ${workspaceRoot}/..
 
-python3 -m instant_ngp_3dml compute_scene 0223-1120
+python3 -m instant_ngp_3dml compute_scene lego
