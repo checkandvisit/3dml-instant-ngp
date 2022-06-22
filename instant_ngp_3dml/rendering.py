b10e3c49bac33a38006c11df52b8cc91a6d563df
Thomas Rouch <thom01.rouch@gmail.com>
2022-06-22 17:55:30 +0200

    :sparkles: render topview in "compute_scene"

--- instant_ngp_3dml/rendering.py
+++ instant_ngp_3dml/rendering.py
@@ -14,6 +14,8 @@ from instant_ngp_3dml.utils.profiler import profile
 from instant_ngp_3dml.utils.tonemapper import linear_to_srgb
 
 
+RENDER_MODES: Dict[str, ngp.RenderMode] = {"depth": ngp.RenderMode.Depth,
+                                           "color": ngp.RenderMode.Shade}
 CAMERA_MODES: Dict[str, ngp.CameraMode] = {"perspective": ngp.CameraMode.Perspective,
                                            "orthographic": ngp.CameraMode.Orthographic,
                                            "environment": ngp.CameraMode.Environment}
@@ -40,26 +42,26 @@ def __save(outname, image):
 
 
 @profile
-def render(load_snapshot: str,
-           screenshot_transforms: str,
-           screenshot_dir: str,
+def render(snapshot_msgpack: str,
+           transforms_json: str,
+           output_dir: str,
            width: int = 1920,
            height: int = 1080,
-           depth: bool = False,
-           screenshot_spp: int = 4,
+           spp: int = 4,
            display: bool = False,
            num_max_images: int = -1,
-           camera_mode: str = "perspective"):
+           render_mode: str = "color",
+           camera_mode: str = "perspective") -> str:
     """Nerf renderer"""
-    # pylint: disable=too-many-arguments
-
+    # pylint: disable=too-many-arguments,too-many-statements,too-many-branches
     testbed = ngp.Testbed(ngp.TestbedMode.Nerf)
 
-    logger.info(f"Loading snapshot {load_snapshot}")
-    testbed.load_snapshot(load_snapshot)
+    logger.info(f"Loading snapshot {snapshot_msgpack}")
+    assert snapshot_msgpack.endswith(".msgpack")
+    testbed.load_snapshot(snapshot_msgpack)
 
-    logger.debug(f"Screenshot transforms from {screenshot_transforms}")
-    ref_transforms = read_json(screenshot_transforms)
+    logger.debug(f"Load rendering transforms from {transforms_json}")
+    ref_transforms = read_json(transforms_json)
 
     # Pick a sensible GUI resolution depending on arguments.
     sw = width
@@ -79,7 +81,7 @@ def render(load_snapshot: str,
 
     testbed.exposure = 1.0
     testbed.fov_axis = 0
-    testbed.fov = ref_transforms["camera_angle_x"] * 180 / np.pi
+    testbed.fov = np.rad2deg(ref_transforms["camera_angle_x"])
     testbed.dynamic_res = False
     testbed.fixed_res_factor = 1
 
@@ -87,43 +89,54 @@ def render(load_snapshot: str,
         f"Invalid camera mode '{camera_mode}'. Should be in {CAMERA_MODES.keys()}"
     testbed.camera_mode = CAMERA_MODES[camera_mode.lower()]
 
-    if depth:
+    assert render_mode.lower() in RENDER_MODES, \
+        f"Invalid render mode '{render_mode}'. Should be in {RENDER_MODES.keys()}"
+    testbed.render_mode = RENDER_MODES[render_mode.lower()]
+
+    if render_mode == "depth":
         logger.info("Set depth rendering params")
         testbed.tonemap_curve = ngp.TonemapCurve.Identity
         testbed.color_space = ngp.ColorSpace.Linear
         testbed.render_mode = ngp.RenderMode.Depth
         testbed.exposure = -4.0
-        screenshot_spp = 1
+        spp = 1
 
     nb_frames = len(ref_transforms["frames"])
     if num_max_images > 0:
         nb_frames = min(num_max_images, nb_frames)
+
+    images_dir = os.path.join(output_dir, f"{render_mode}_{camera_mode}")
+    if display:
+        images_dir += "_gl"
+    os.makedirs(images_dir, exist_ok=True)
+
     with tqdm(desc="Rendering", total=nb_frames, unit="frame") as t:
         for idx in range(nb_frames):
             f = ref_transforms["frames"][int(idx)]
             cam_matrix = f["transform_matrix"]
             testbed.set_nerf_camera_matrix(np.matrix(cam_matrix)[:-1, :])
-            outname = os.path.join(
-                screenshot_dir, os.path.basename(f["file_path"]))
+            outname = os.path.join(images_dir, os.path.basename(f["file_path"]))
 
             if display:
                 testbed.reset_accumulation()
-                for _ in range(screenshot_spp):
+                for _ in range(spp):
                     testbed.frame()
 
                 image = testbed.screenshot()
             else:
-                image = testbed.render(sw, sh, screenshot_spp, True)
+                image = testbed.render(sw, sh, spp, True)
                 image[..., :3] *= 2**(-1*testbed.exposure)
 
-            if depth:
+            if render_mode == "color":
+                __save(outname, image)
+            elif render_mode == "depth":
                 # Force depth in numpy format
                 outname = os.path.splitext(outname)[0] + ".npy"
                 os.makedirs(os.path.dirname(outname), exist_ok=True)
                 np.save(outname, image[..., 0])
-                # No need to correct exposure on depth
-
             else:
-                __save(outname, image)
+                raise ValueError(f"Invalid render mode '{render_mode}'. Should be in {RENDER_MODES.keys()}")
 
             t.update(1)
+
+    return images_dir
